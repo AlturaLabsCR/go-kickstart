f3a35b69901c286d013470d1b50a0c42b378742b
tavocg <gustavo.calvo.cr@gmail.com>
2026-05-04 03:35:19 -0600

    interface

---
+++ database/database.go
@@ -0,0 +1,17 @@
+// Package database
+package database
+
+import "context"
+
+type Database interface {
+	Querier() Querier
+	WithTx(ctx context.Context, fn func(q Querier) error) (err error)
+	Exec(ctx context.Context, sql string) (err error)
+	Close(ctx context.Context) (err error)
+}
+
+type Querier interface {
+	// OncesertAccountByEmail creates an account by email if not yet exists.
+	// It returns the email's subject.
+	OncesertAccountByEmail(ctx context.Context, email string) (sub string, err error)
+}
