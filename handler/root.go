5c349e4daeaef706746ed57f4b33f09d204d4277
tavocg <gustavo.calvo.cr@gmail.com>
2026-05-08 16:21:53 -0600

    if dev

--- handler/root.go
+++ handler/root.go
@@ -8,5 +8,10 @@ func init() {
 
 func Root(w http.ResponseWriter, r *http.Request) {
 	handler.logger.Info("hit root")
+	if handler.dev {
+		_, _ = w.Write([]byte("ok (dev)\n"))
+		return
+	}
+
 	_, _ = w.Write([]byte("ok\n"))
 }
