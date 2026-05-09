f212bbad0876c51112cb0cbfe35972645c59db9f
tavocg <gustavo.calvo.cr@gmail.com>
2026-05-09 01:57:04 -0600

    custom returns

--- handlers/root.go
+++ handlers/root.go
@@ -1,6 +1,13 @@
 package handlers
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+	"strings"
+
+	"github.com/myrepo/myserver/templates/base"
+	rootpage "github.com/myrepo/myserver/templates/root"
+)
 
 func (h *Handler) registerRootRoutes() {
 	h.Add(http.MethodGet, "/", h.Root)
@@ -8,5 +15,36 @@ func (h *Handler) registerRootRoutes() {
 
 func (h *Handler) Root(w http.ResponseWriter, r *http.Request) {
 	L := h.localizer.LocalizerFunc(h.localizer.PickLanguageFromRequest(r))
-	_, _ = w.Write([]byte(L("root.greeting") + "\n"))
+
+	if acceptsHTML(r) {
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		page := base.Page(L, base.PageParams{
+			Head: base.HeadParams{
+				Title:                 "MyServer",
+				Subtitle:              L("root.greeting"),
+				Description:           L("root.greeting"),
+				RobotsIndex:           true,
+				RobotsGoogleTranslate: true,
+			},
+			Body: base.BodyParams{
+				Content: rootpage.RootMain(L),
+				Active:  "/",
+			},
+		})
+
+		if err := page.Render(r.Context(), w); err != nil {
+			http.Error(w, "failed to render root page", http.StatusInternalServerError)
+		}
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	_ = json.NewEncoder(w).Encode(map[string]string{
+		"message": L("root.greeting"),
+	})
+}
+
+func acceptsHTML(r *http.Request) bool {
+	accept := r.Header.Get("Accept")
+	return strings.Contains(accept, "text/html") || strings.Contains(accept, "application/xhtml+xml")
 }
