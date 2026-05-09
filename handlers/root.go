ae24c64a7dd12c958039cbf777efdf366888a260
tavocg <gustavo.calvo.cr@gmail.com>
2026-05-09 01:19:56 -0600

    use localizer

--- handlers/root.go
+++ handlers/root.go
@@ -7,10 +7,6 @@ func (h *Handler) registerRootRoutes() {
 }
 
 func (h *Handler) Root(w http.ResponseWriter, r *http.Request) {
-	if h.dev {
-		_, _ = w.Write([]byte("ok (dev)\n"))
-		return
-	}
-
-	_, _ = w.Write([]byte("ok\n"))
+	L := h.localizer.LocalizerFunc(h.localizer.PickLanguageFromRequest(r))
+	_, _ = w.Write([]byte(L("root.greeting") + "\n"))
 }
