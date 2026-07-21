f4fb660afd4cecd4a855f5aafca6b6e0473a17db
tavocg <gustavo.calvo.cr@gmail.com>
2026-07-21 12:37:46 -0600

    chore: add cache to handler options

--- handlers/handler.go
+++ handlers/handler.go
@@ -8,6 +8,7 @@ import (
 	"strings"
 
 	appauth "app/auth"
+	"app/cache"
 	"app/database"
 	"app/middleware"
 	"github.com/tavocg/go-auth"
@@ -27,6 +28,7 @@ type Handler struct {
 
 	logger        Logger
 	db            database.Database
+	cache         cache.Cache
 	authenticator auth.Authenticator[*appauth.Claims]
 	localizer     *i18n.Localizer
 	rootPrefix    string
@@ -39,6 +41,7 @@ type Options struct {
 	Logger        Logger
 	Dev           bool
 	DB            database.Database
+	Cache         cache.Cache
 	Authenticator auth.Authenticator[*appauth.Claims]
 	Localizer     *i18n.Localizer
 	RootPrefix    string
@@ -60,6 +63,11 @@ func NewHandler(opts Options) *Handler {
 		panic("handler authenticator is required")
 	}
 
+	c := opts.Cache
+	if c == nil {
+		c = cache.NewMemoryCache("app")
+	}
+
 	rootPrefix := normalizeRootPrefix(opts.RootPrefix)
 
 	next := &Handler{
@@ -67,6 +75,7 @@ func NewHandler(opts Options) *Handler {
 		dev:           opts.Dev,
 		logger:        logger,
 		db:            opts.DB,
+		cache:         c,
 		authenticator: authenticator,
 		localizer:     localizer,
 		rootPrefix:    rootPrefix,
